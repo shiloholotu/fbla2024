752578bbe1574117269c21b67c7ca8e269c99f89
Nolan Yee <116700450+nolanqyee@users.noreply.github.com>
2024-01-22 03:16:44 +0000

    Created job listing pages, fixed navbar homepage bug

--- app.py
+++ app.py
@@ -11,6 +11,17 @@ def index():
 def demo():
     return render_template("demo.html")
 
+@app.route("/engineer")
+def engineer():
+    return render_template("engineer.html")
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
+
+@app.route("/risk")
+def risk():
+    return render_template("risk.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

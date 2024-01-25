1763d302f86a85ac9fb083cd92b66ff918d4c221
Nolan Yee <116700450+nolanqyee@users.noreply.github.com>
2024-01-25 03:11:48 +0000

    fixing typos

--- static/scripts/slideshow.js
+++ static/scripts/slideshow.js
@@ -4,22 +4,22 @@ const slides = [
         "Unleash your artistic <span class='purpText'>Creativity.</span>",
         "mona.png",
         `Here at Singularity, we work to enable creativity and expression through our Image-Generation Service. Our users can use natural language prompts to generate new images, 
-        combine unique styles, and find inspiration for their own art. The service functions through a Generative Adversarial Network(GAN), 
-        a system of neural networks that can generate completely original digital images from a dataset of publically-available images. We understand the importance of originality 
-        and privacy, so we allow any artist or photographer opt out their publically-available images from our GAN's training process. `
+        combine unique styles, and find inspiration for their own art. The service functions through a Generative Adversarial Network (GAN), 
+        a system of neural networks that can generate completely original digital images from a dataset of publicly-available images. We understand the importance of originality 
+        and privacy, so we allow any artist or photographer to opt out their publicly-available images from our GAN's training process. `
     ],
     [
         "Write Content <span class='purpText'>Effortlessly.</span>",
         "text-gen.png",
-        `With Singularity, any user can us or Text-Generation Service to find inspiration, improve their own writing, or find answers to a vast range of questions.
-        Our text-generation service utilizes a large language model(LLM) heavily trained on years of text data. The LLM is built in with a Transformer neural netweork structure, which
+        `With Singularity, any user can use our Text-Generation Service to find inspiration, improve their own writing, or find answers to a vast range of questions.
+        Our text-generation service utilizes a large language model (LLM) heavily trained on years of text data. The LLM is built in with a Transformer neural network structure, which
         allows the model to process multiple inputs in parallel, make the model capable of understanding human prompts and producing natural language outputs. 
-        LLMs are one of the most innovative and fast-growing apsects of artificial inteligence and here at Singularity, we aim to ensure a future where AI can serve as a tool for the progresion of society.`
+        LLMs are one of the most innovative and fast-growing aspects of artificial intelligence, and, here at Singularity, we aim to ensure a future where AI can serve as a tool for the progression of society.`
     ],
     [
         "Programming <span class='purpText'>Made Easy.</span>",
         "code.png",
-        `Writing code has never been easier with Singularity's code-generation service. The service is built on a large language model(LLM) specifically trained on publically available code.
+        `Writing code has never been easier with Singularity's code-generation service. The service is built on a large language model (LLM) specifically trained on publicly available code.
         Our model allows for fully-functional snippets of code to be generated solely through natural language prompts. Singularity supports a wide variety of languages, including C++, Python,
          Java, Javascript, HTML, CSS, C#, and others.`
     ]

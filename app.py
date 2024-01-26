e3361a9f60c1472609649144fa7c4734469b1038
Shiloh Olotu <shiloholotu@gmail.com>
2024-01-26 16:47:42 -0600

    deleted old firebase code

--- app.py
+++ app.py
@@ -14,55 +14,7 @@ def decodeHex(hstr):
     return bytes.fromhex(hstr).decode("utf-8")
 
 
-'''
-# Initialize Firestore DB
-cred = credentials.Certificate('key.json')
-default_app = initialize_app(cred)
-db = firestore.client()
-todo_ref = db.collection('todos')
 
-@app.route('/add', methods=['POST'])
-def create():
-    try:
-        id = request.json['id']
-        todo_ref.document(id).set(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-    
-@app.route('/list', methods=['GET'])
-def read():
-    try:
-        # Check if ID was passed to URL query
-        todo_id = request.args.get('id')    
-        if todo_id:
-            todo = todo_ref.document(todo_id).get()
-            return jsonify(todo.to_dict()), 200
-        else:
-            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
-            return jsonify(all_todos), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-    
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
-    try:
-        id = request.json['id']
-        todo_ref.document(id).update(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-    
-@app.route('/delete', methods=['GET', 'DELETE'])
-def delete():
-    try:
-        # Check for ID in URL query
-        todo_id = request.args.get('id')
-        todo_ref.document(todo_id).delete()
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-'''
 @app.route("/")
 def index():
     return render_template("index.html")

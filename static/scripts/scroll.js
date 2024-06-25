782f68d7d332dff19e940145a588f84d07e5fb46
Shiloh Olotu <shiloholotu@gmail.com>
2024-06-25 08:51:59 -0500

    Finish home page revamps

--- static/scripts/scroll.js
+++ static/scripts/scroll.js
@@ -21,8 +21,8 @@ window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
 
     if(scrollPosition >= 2651){
-        scrollButton.innerHTML = "<img src='static/assets/svg/down.png' style='width:17px;opacity:.6;transform:translate(0,2px)'>";
+        scrollButton.innerHTML = "<img src='static/assets/svg/up.png' style='transform:translate(0,0)'>";
     } else {
-        scrollButton.innerHTML = "<img src='static/assets/svg/down.png' style='width:17px;opacity:.6;transform:translate(0,2px)'>";
+        scrollButton.innerHTML = "<img src='static/assets/svg/down.png'>";
     }
 })
